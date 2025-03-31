e4e60162df9ef1a7133da47adddc8f1deacf7bb0
Guirao <7947346@alu365.murciaeduca.es>
2025-03-31 13:32:58 +0200

    1º commit. Creación del proyecto.

---
+++ src/test/java/viewnext/bunch/BunchApplicationTests.java
@@ -0,0 +1,13 @@
+package viewnext.bunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
