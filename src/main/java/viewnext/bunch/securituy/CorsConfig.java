41412c834f1e1224084763e56b99f99a38d6f341
Guirao <daniel.guirao@viewnext.com>
2025-04-02 15:01:44 +0200

    8º commit. CorsConfig y docker-compose.yml 1.2

---
+++ src/main/java/viewnext/bunch/securituy/CorsConfig.java
@@ -0,0 +1,26 @@
+package viewnext.bunch.securituy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author Daniel Guirao
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*"); // Permitir todos los orígenes, puedes especificar los orígenes permitidos
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
